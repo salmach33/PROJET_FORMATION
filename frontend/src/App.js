665fe2c986ba6a240f35a7e033866475892266bb
Salma33 <salmachlarhmo@gmail.com>
2026-04-21 11:41:44 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <p>Hello world</p>
+       <p>Hello salma</p>
       </header>
     </div>
   );
